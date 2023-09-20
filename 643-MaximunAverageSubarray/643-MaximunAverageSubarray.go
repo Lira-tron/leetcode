2b2c7f596b83b87a321d75433e81541cc92c5760
Liratron <octalimon@gmail.com>
2023-09-20 08:57:52 -0700

    improving 643

--- 643-MaximunAverageSubarray/643-MaximunAverageSubarray.go
+++ 643-MaximunAverageSubarray/643-MaximunAverageSubarray.go
@@ -3,18 +3,18 @@ package leetcode
 import "math"
 
 func findMaxAverage(nums []int, k int) float64 {
-	var max float64 = float64(-1 << 63)
+	var max int = math.MinInt
 	var sum int
 	for i, v := range nums {
 		if i >= k {
 			sum -= nums[i-k]
 		}
 		sum += v
-		if i >= k-1 {
-			max = math.Max(max, float64(sum)/float64(k))
+		if i >= k-1 && sum > max {
+			max = sum
 		}
 	}
-	return roundFloat(max, 5)
+	return roundFloat(float64(max)/float64(k), 5)
 }
 
 func roundFloat(val float64, precision int) float64 {
